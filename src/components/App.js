539da74849f02bfcef1c009242ad8af4bc3c96e9
Brian Ashman <brian.ashman@comcast.net>
2023-06-06 22:22:53 -0700

    Complete State and Events Mini Project

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  console.log(tasks);
+
+  function handleClick(category) {
+    setSelectedCategory(category);
+  }
+
+  function handleDeleteTask(index) {
+    setTasks((tasks) => {
+      const newTaskList = [...tasks];
+      newTaskList.splice(index, 1);
+      return newTaskList;
+    });
+  }
+
+  function handleNewTask(newTaskData) {
+    setTasks([...tasks, newTaskData]);
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} onFilter={handleClick} />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleNewTask}/>
+      <TaskList tasks={tasks} onDelete={handleDeleteTask} selectedCategory={selectedCategory} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, onDelete, selectedCategory }) {
+
+  const filteredTasks = tasks.filter((task) => {
+    if (selectedCategory === "All") return true;
+    return task.category === selectedCategory
+  })
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {filteredTasks.map((task, index) => (
+        <Task key={index} category={task.category} text={task.text} onDelete={() => onDelete(index)} />
+      ))}
     </div>
   );
 }
